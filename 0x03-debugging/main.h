8918370b46364389abab727e8718b4a6da8280c7
Satope <qsatope@gmail.com>
2022-04-15 14:20:26 -0700

    creating main.h, main.c file

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,8 +4,9 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-void print_remau=ining_days(int month, int day, int year);
-int convert_day(int month, int day);
+  int largest_number(int a, int b, int c);
+   void print_remau=ining_days(int month, int day, int year);
+   int convert_day(int month, int day);
+
 
 #endif
